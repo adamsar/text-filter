21a1ee291994a532261f7e29fb5e5c075ba0c008
Andrew Adams <adamsar@gmail.com>
2015-02-15 16:42:13 +0900

    Basic app functionality

--- text_filter/api.py
+++ text_filter/api.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, make_response
+from text_filter.massage import massage
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,7 +7,10 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/modify_text", methods=["POST"])
+@app.route("/filter", methods=["POST"])
 def modify_text():
-    #TODO
-    pass
+    text = request.form['text']
+    filtered_text = massage(text)
+    response = make_response(filtered_text)
+    response.headers['Content-Disposition'] = 'attachment; filename=filtered.txt'
+    return response
